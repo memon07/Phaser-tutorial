8a0a394701c7ebbff90eacbd9ad200b4e6e11966
memon07 <memonshoyeb26@gmail.com>
2019-05-05 00:21:25 +0530

    lesson 3

--- BikeGame/Lesson3/js/main.js
+++ BikeGame/Lesson3/js/main.js
@@ -3,8 +3,9 @@ var game = new Phaser.Game(800,600,Phaser.AUTO);
 var GameState = {
     preload : function() {
         this.load.image('background','/BIKE/lesson3/assets/images/background-grass.png');
-
-        this.load.spritesheet('boy','/BIKE/lesson3/assets/images/boy.png',131,131,7);
+        // this.load.spritesheet('boy','/BIKE/lesson3/assets/images/boyc.png',131,131,7);
+        this.load.spritesheet('girl','/BIKE/lesson3/assets/images/girl.webp',211,251,6);
+        this.load.audio('scream','/BIKE/lesson3/assets/audio/boyScream.mp3')
     },
     create : function() {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -13,15 +14,20 @@ var GameState = {
 
         this.background = this.game.add.sprite(0, 0, 'background');
 
-        this.boy = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'boy',0);
-        this.boy.anchor.setTo(0.5,-0.05);
-        // this.boy.scale.setTo(0.65);
+        this.girl = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'girl',0);
+        this.girl.anchor.setTo(0.5,-0.05);
+        fx = game.add.audio('scream');
+        // this.girl.scale.setTo(0.65);
 
-        this.boy.animations.add('animate',[0,1,2,3,4,5,6],3,false);
+        this.girl.animations.add('animate',[0,1,2,3,4,5,6],3,false);
 
-        this.boy.inputEnabled = true;
-        this.boy.input.pixelPerfectClick = true;
-        this.boy.events.onInputDown.add(this.animateBike, this);
+        this.girl.inputEnabled = true;
+        this.girl.input.pixelPerfectClick = true;
+        this.girl.events.onInputDown.add(this.animateBike, this);
+
+        var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+        var text = game.add.text(game.world.centerX, game.world.centerY, "GIRL RUNNING", style);
+        text.anchor.set(1.5);
     },
     update : function() {
     },
@@ -29,6 +35,7 @@ var GameState = {
     },
     animateBike: function(sprite,event){
         sprite.play('animate');
+        fx.play();
     }
 
 };
