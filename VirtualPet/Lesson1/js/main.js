8b5c5820c131ad5cd1c47bb9d83033dbf7181854
memon07 <memonshoyeb26@gmail.com>
2019-05-06 23:56:56 +0530

    virtual pet main

--- VirtualPet/Lesson1/js/main.js
+++ VirtualPet/Lesson1/js/main.js
@@ -1,6 +1,9 @@
 var GameState = {
     init: function(){
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
     },
     preload: function(){
 
