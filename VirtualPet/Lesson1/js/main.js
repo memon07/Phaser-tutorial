becd7ee86025ddd0aef73d25f62849019454838f
memon07 <memonshoyeb26@gmail.com>
2019-05-13 00:45:57 +0530

    added assets and background in game

--- VirtualPet/Lesson1/js/main.js
+++ VirtualPet/Lesson1/js/main.js
@@ -6,10 +6,19 @@ var GameState = {
         this.scale.pageAlignVertically = true;
     },
     preload: function(){
-
+        this.load.image('candy', '/VIRTUALPET/Lesson1/assets/images/candy.png');
+        this.load.image('apple', '/VIRTUALPET/Lesson1/assets/images/apple.png');
+        this.load.image('arrow', '/VIRTUALPET/Lesson1/assets/images/arrow.png');
+        this.load.image('background', '/VIRTUALPET/Lesson1/assets/images/bg.png');
+        this.load.spritesheet('boy', '/VIRTUALPET/Lesson1/assets/images/boyTileset.png', 400,400,4,2,2);
     },
     create: function(){
+        this.background = this.game.add.sprite(0,0,'background');
 
+        this.boy = this.game.add.sprite(100,400,'boy');
+        this.boy.anchor.setTo(0.5);
+        
+        this.boy.customParams = { health: 100 , fun : 100 };
     }
 }
 
